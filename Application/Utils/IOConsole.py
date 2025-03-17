3ed0e3b198468fb4a16a75c2c5decb688d0691d4
Daniel Moffett <moffd234@gmail.com>
2025-03-17 17:17:25 -0400

    Refactored check_for_exit to return instead directly exiting

--- Application/Utils/IOConsole.py
+++ Application/Utils/IOConsole.py
@@ -5,15 +5,17 @@ class IOConsole:
     def __init__(self, color:ANSI_COLORS=ANSI_COLORS.RED):
         if not isinstance(color, ANSI_COLORS):
             raise TypeError("color must be an instance of ANSI_COLORS")
-        self.color:ANSI_COLORS = color
+        self.color:str = color.value
 
     def get_string_input(self, prompt: str) -> str:
         user_input = input(self.color + prompt)
+        if self.check_for_exit(user_input) == 1:
+            exit(0)
         self.check_for_exit(user_input)
         return user_input
 
-    def check_for_exit(self, user_input: str):
+    def check_for_exit(self, user_input: str) -> int:
         if user_input.lower() == "exit":
             print(self.color + "Exiting the game")
-            exit(0)
-
+            return 1
+        return 0
