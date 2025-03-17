f6082f71e03435709b817def2d51090e83a0b876
Daniel Moffett <moffd234@gmail.com>
2025-03-17 16:34:04 -0400

    Added ANSI_COLORS for terminal text coloring

---
+++ Application/Utils/ANSI_COLORS.py
@@ -0,0 +1,10 @@
+class ANSI_COLORS:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\0尓3[37m'
+    RESET = '\033[0m' # Reset to default color
