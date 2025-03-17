ef20e7c4a84e4a50f62e7a0126df35464eaff3ba
Daniel Moffett <moffd234@gmail.com>
2025-03-17 16:49:53 -0400

    Added ANSI_COLORS for terminal text coloring

--- Application/Utils/IOConsole.py
+++ Application/Utils/IOConsole.py
@@ -2,16 +2,16 @@ from Application.Utils.ANSI_COLORS import ANSI_COLORS
 
 
 class IOConsole:
-    def __init__(self, color=ANSI_COLORS.RED):
+    def __init__(self, color:ANSI_COLORS=ANSI_COLORS.RED):
         self.color:str = color
 
     def get_string_input(self, prompt: str) -> str:
-        user_input = input(prompt)
+        user_input = input(self.color + prompt)
         self.check_for_exit(user_input)
         return user_input
 
     def check_for_exit(self, user_input: str):
         if user_input.lower() == "exit":
-            print(self.color + "Exiting the game" + ANSI_COLORS.RESET)
+            print(self.color + "Exiting the game")
             exit(0)
 
