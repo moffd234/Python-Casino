ece2d0d87a5ee69d84621e14479c13a05bfaac27
Daniel Moffett <moffd234@gmail.com>
2025-03-18 16:46:13 -0400

    Wrote tests for subtract_losses

--- Tests/Casino/CasinoAccountTest.py
+++ Tests/Casino/CasinoAccountTest.py
@@ -35,3 +35,23 @@ class CasinoAccountTest(unittest.TestCase):
     def test_add_winnings_negative(self):
         with self.assertRaises(ValueError):
             self.account.add_winnings(-1)
+
+    def test_subtract_losses(self):
+        self.account.balance = 50
+        expected = self.account.balance - 20
+        actual = self.account.subtract_losses(20)
+        self.assertEqual(actual, expected)
+
+    def test_subtract_losses_full_balance(self):
+        self.account.balance = 50
+        self.account.subtract_losses(50)
+        self.assertEqual(self.account.balance, 0)
+
+    def test_subtract_losses_negative(self):
+        with self.assertRaises(ValueError):
+            self.account.subtract_losses(-5)
+
+    def test_subtract_losses_insufficient_funds(self):
+        self.account.balance = 50
+        with self.assertRaises(ValueError):
+            self.account.subtract_losses(100)
