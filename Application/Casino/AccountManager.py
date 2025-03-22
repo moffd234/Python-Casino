65bab5f7290d7f9987d01ba5c71ba511c1cc5412
Daniel Moffett <moffd234@gmail.com>
2025-03-22 16:48:48 -0400

    Create AccountManager

---
+++ Application/Casino/AccountManager.py
@@ -0,0 +1,14 @@
+from Application.Casino.CasinoAccount import CasinoAccount
+
+
+class AccountManager:
+    def __init__(self):
+        self.accounts: [CasinoAccount] = None
+
+
+    def create_account(self, username: str, password: str) -> CasinoAccount | None:
+        for account in self.accounts:
+            if account.username == username:
+                return None
+
+        return CasinoAccount(username, password)
