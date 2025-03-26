0fe945162a9a56e25c0d460ca8358873741e5afe
Daniel Moffett <moffd234@gmail.com>
2025-03-26 18:28:43 -0400

    Wrote handle_login

--- Application/Casino/Casino.py
+++ Application/Casino/Casino.py
@@ -1,7 +1,10 @@
+from Application.Casino.AccountManager import AccountManager
+from Application.Casino.CasinoAccount import CasinoAccount
 from Application.Utils.ANSI_COLORS import ANSI_COLORS
 from Application.Utils.IOConsole import IOConsole
 
 IOConsole = IOConsole()
+manager = AccountManager()
 
 def print_welcome():
     return IOConsole.print_colored(r"""
@@ -14,3 +17,15 @@ def print_welcome():
         8888P   Y8888 Y8b.     888 Y88b.   Y88..88P 888  888  888 Y8b.      "   "  
         888P     Y888  "Y8888  888  "Y8888P "Y88P"  888  888  888  "Y8888  888 888
         """, ANSI_COLORS.BLUE)
+
+def handle_login() -> CasinoAccount:
+    username: str = IOConsole.get_string_input("Enter your username\n")
+    password: str = IOConsole.get_string_input("Enter your password\n")
+
+    account: CasinoAccount | None = manager.get_account(username=username, password=password)
+    while True:
+        if account:
+            return account
+        else:
+            print("Invalid login info")
+
