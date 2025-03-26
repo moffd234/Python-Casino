9fcf2e2e9be382d503fa7e8bc7a5b3f68d795fda
Daniel Moffett <moffd234@gmail.com>
2025-03-26 16:48:56 -0400

    Wrote read_from_csv

--- Application/Casino/AccountManager.py
+++ Application/Casino/AccountManager.py
@@ -1,18 +1,29 @@
 import csv
 from Application.Casino.CasinoAccount import CasinoAccount
 
+FP = "./accounts.csv"
 
 def write_new_account_to_csv(account: CasinoAccount):
     account_details: list = [account.username, account.password, account.balance]
 
-    fp = "./accounts.csv"
-    with open(fp, "a", newline='') as file:
+    with open(FP, "a", newline='') as file:
         writer = csv.writer(file)
         writer.writerow(account_details)
 
+
+def read_from_csv() -> list[CasinoAccount]:
+    accounts: list = []
+    with open(FP, "r") as file:
+        csv_reader = csv.reader(file)
+        for line in csv_reader:
+            new_account = CasinoAccount(line[0], line[1], float(line[2]))
+            accounts.append(new_account)
+    return accounts
+
+
 class AccountManager:
     def __init__(self):
-        self.accounts: [CasinoAccount] = []
+        self.accounts: [CasinoAccount] = read_from_csv()
 
 
     def create_account(self, username: str, password: str) -> CasinoAccount | None:
