192ddb5d3c1ec3ccf6b7fa461965ec7ed5f9fa9c
Daniel Moffett <moffd234@gmail.com>
2025-03-30 16:17:16 -0400

    Wrote print_welcome_message for CoinFlip

--- Application/Casino/Games/CoinFlip.py
+++ Application/Casino/Games/CoinFlip.py
@@ -1,13 +1,27 @@
 from Application.Casino.CasinoAccount import CasinoAccount
 from Application.Casino.Games.Game import Game
+from Application.Utils.ANSI_COLORS import ANSI_COLORS
 
 
 class CoinFlip(Game):
-    def print_welcome_message(self) -> str:
-        pass
-
-    def run(self):
-        pass
 
     def __init__(self, player: CasinoAccount):
         super().__init__(player)
+        self.console.color = ANSI_COLORS.BLUE.value
+
+    def print_welcome_message(self) -> str:
+        return self.console.print_colored(r"""
+        
+        Yb        dP 888888 88      dP""b8  dP"Yb  8b    d8 888888     888888  dP"Yb       dP""b8  dP"Yb  88 88b 88     888888 88     88 88""Yb 
+         Yb  db  dP  88__   88     dP   `" dP   Yb 88b  d88 88__         88   dP   Yb     dP   `" dP   Yb 88 88Yb88     88__   88     88 88__dP 
+          YbdPYbdP   88""   88  .o Yb      Yb   dP 88YbdP88 88""         88   Yb   dP     Yb      Yb   dP 88 88 Y88     88""   88  .o 88 88
+           YP  YP    888888 88ood8  YboodP  YbodP  88 YY 88 888888       88    YbodP       YboodP  YbodP  88 88  Y8     88     88ood8 88 88     
+        
+        rules:
+             1. Enter a guess of either heads or tails
+             2. A coin will be flipped
+             3. If you guess correctly you will win 1.25x your wager
+        """)
+
+    def run(self):
+        pass
