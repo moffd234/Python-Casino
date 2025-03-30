e5ca18d867669df42111afc2b1d063a39e874df0
Daniel Moffett <moffd234@gmail.com>
2025-03-29 21:36:29 -0400

    Added methods to Game class

--- Tests/Casino/Games/Game.py
+++ Tests/Casino/Games/Game.py
@@ -1,7 +1,27 @@
+from abc import ABC, abstractmethod
+
 from Application.Casino.CasinoAccount import CasinoAccount
+from Application.Utils.IOConsole import IOConsole
 
 
-class Game:
+class Game(ABC):
     def __init__(self, player: CasinoAccount):
-
         self.player = player
+        self.console = IOConsole()
+
+    @abstractmethod
+    def print_welcome_message(self) -> float:
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    def get_wager_amount(self):
+        amount: float = self.console.get_float_input("Enter a wager amount")
+
+        while amount > self.player.balance or amount < 1.00:
+            print(f"Wager amount must be between $1.00 - {self.player.balance}")
+            amount = self.console.get_float_input("Enter a wager amount")
+
+        return amount
