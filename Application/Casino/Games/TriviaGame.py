acbddbe6eeb8caa78f83dd9870d9455b6fa26ac2
moffd234 <159962112+moffd234@users.noreply.github.com>
2025-03-31 15:42:22 -0400

    Wrote get_response

--- Application/Casino/Games/TriviaGame.py
+++ Application/Casino/Games/TriviaGame.py
@@ -1,3 +1,5 @@
+import requests
+
 from Application.Casino.CasinoAccount import CasinoAccount
 from Application.Casino.Games.Game import Game
 
@@ -12,3 +14,12 @@ class TriviaGame(Game):
 
     def __init__(self, player: CasinoAccount):
         super().__init__(player)
+
+    def get_response(self, url) -> None | dict:
+        response = requests.get(url)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print("Problem getting questions. Please try again later.")
+            return None
+        return response.json()
