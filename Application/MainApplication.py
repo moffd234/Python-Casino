6cb4341ffc9730ca818bea04c6db5d460e8d1a9e
moffd234 <159962112+moffd234@users.noreply.github.com>
2025-03-31 19:51:33 -0400

    Added each game to their own directory

--- Application/MainApplication.py
+++ Application/MainApplication.py
@@ -1,5 +1,9 @@
-from Application.Casino.Casino import Casino
+from Application.Casino.AccountManager import AccountManager
+from Application.Casino.Games.TriviaGame.TriviaGame import TriviaGame
 
 if __name__ == '__main__':
-    casino = Casino()
-    casino.run()
+    # casino = Casino()
+    # casino.run()
+    manager = AccountManager()
+    game = TriviaGame(manager.get_account("Username", "Password"))
+    game.get_difficulty()
