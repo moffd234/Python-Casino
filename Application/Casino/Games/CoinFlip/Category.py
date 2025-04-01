e6df0a381a7b193342c39aa70c061513d14b3932
moffd234 <159962112+moffd234@users.noreply.github.com>
2025-03-31 20:16:13 -0400

    Added type hints to Category.py

--- Application/Casino/Games/CoinFlip/Category.py
+++
@@ -1,7 +0,0 @@
-class Category:
-    def __init__(self, name, id_num, easy_num, med_num, hard_num):
-        self.name = name
-        self.id = id_num
-        self.easy_num = easy_num
-        self.med_num = med_num
-        self.hard_num = hard_num
---
+++ Application/Casino/Games/TriviaGame/Category.py
@@ -0,0 +1,7 @@
+class Category:
+    def __init__(self, name: str, id_num: int, easy_num: int, med_num: int, hard_num: int):
+        self.name: str = name
+        self.id: int = id_num
+        self.easy_num: int = easy_num
+        self.med_num: int = med_num
+        self.hard_num: int = hard_num
