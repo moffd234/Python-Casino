3cdcc68b6cc11c490d2cc515df7c93731673daff
Daniel Moffett <moffd234@gmail.com>
2025-04-01 19:10:59 -0400

    Wrote to_dict for Category.py

--- Application/Casino/Games/TriviaGame/Category.py
+++ Application/Casino/Games/TriviaGame/Category.py
@@ -5,3 +5,12 @@ class Category:
         self.easy_num: int = easy_num
         self.med_num: int = med_num
         self.hard_num: int = hard_num
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "id": self.id,
+            "easy_num": self.easy_num,
+            "med_num": self.med_num,
+            "hard_num": self.hard_num
+        }
