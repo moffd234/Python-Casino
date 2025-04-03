43c1b7314b086db32db4586736868e71b3500120
moffd234 <159962112+moffd234@users.noreply.github.com>
2025-04-03 16:04:57 -0400

    Created TicTacToe.py

---
+++ Application/Casino/Games/TicTacToe/TicTacToe.py
@@ -0,0 +1,12 @@
+from Application.Casino.Games.Game import Game
+
+
+class TicTacToe(Game):
+    def __init__(self, player):
+        super().__init__(player)
+
+    def print_welcome_message(self) -> str:
+        pass
+
+    def run(self):
+        pass
