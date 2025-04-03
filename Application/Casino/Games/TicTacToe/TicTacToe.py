48d1521fb8618f4ebc0cb85dd83e14ea048c8c5b
moffd234 <159962112+moffd234@users.noreply.github.com>
2025-04-03 16:15:15 -0400

    Wrote print_board

--- Application/Casino/Games/TicTacToe/TicTacToe.py
+++ Application/Casino/Games/TicTacToe/TicTacToe.py
@@ -4,9 +4,18 @@ from Application.Casino.Games.Game import Game
 class TicTacToe(Game):
     def __init__(self, player):
         super().__init__(player)
+        self.game_board = [[" " for _ in range(3)] for _ in range(3)]
+        self.turn = "x"
 
     def print_welcome_message(self) -> str:
         pass
 
     def run(self):
         pass
+
+    def print_board(self) -> None:
+        board_lines = []
+        for row in self.game_board:
+            board_lines.append(" | ".join(row))
+        board_display = "\n---------\n".join(board_lines)
+        print(self.console.print_colored(board_display))
