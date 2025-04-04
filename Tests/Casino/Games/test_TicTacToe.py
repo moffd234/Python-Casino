9468e556fb2a1ca0389726ba5355c88f84b10625
moffd234 <159962112+moffd234@users.noreply.github.com>
2025-04-04 17:25:06 -0400

    Wrote tests for test_check_for_winner_horizontal

--- Tests/Casino/Games/test_TicTacToe.py
+++ Tests/Casino/Games/test_TicTacToe.py
@@ -1,5 +1,41 @@
 import unittest
 
+from Application.Casino.AccountManager import AccountManager
+from Application.Casino.Games.TicTacToe.TicTacToe import TicTacToe
+
 
 class TestTicTacToe(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        manager = AccountManager()
+        self.game = TicTacToe(manager.get_account("Username", "Password"))
+
+    def test_check_for_winner_horizontal(self):
+        self.game.game_board = \
+            [["x", "x", "x"],
+             [" ", " ", " "],
+             [" ", " ", " "]]
+
+        expected: str = "x"
+        actual: str = self.game.check_for_winner()
+        self.assertEqual(expected, actual)
+
+    def test_check_for_winner_horizontal_middle(self):
+        self.game.game_board =\
+            [["o", "x", "x"],
+             ["o", "o", "o"],
+             [" ", " ", " "]]
+
+        expected: str = "o"
+        actual: str = self.game.check_for_winner()
+        self.assertEqual(expected, actual)
+
+    def test_check_for_winner_horizontal_last(self):
+        self.game.game_board =\
+            [["o", "x", "x"],
+             ["o", "o", "o"],
+             ["x", "x", "x"]]
+
+        expected: str = "o"
+        actual: str = self.game.check_for_winner()
+        self.assertEqual(expected, actual)
