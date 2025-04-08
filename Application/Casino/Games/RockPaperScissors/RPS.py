417d1b6071a4b02499a5bbee300d2e4e2cb9ea1f
Daniel Moffett <moffd234@gmail.com>
2025-04-08 03:10:26 -0400

    Created rps.py

---
+++ Application/Casino/Games/RockPaperScissors/RPS.py
@@ -0,0 +1,12 @@
+from Application.Casino.Games.Game import Game
+
+
+class RPS(Game):
+    def print_welcome_message(self) -> str:
+        pass
+
+    def run(self):
+        pass
+
+    def __init__(self, player, manager):
+        super().__init__(player, manager)
