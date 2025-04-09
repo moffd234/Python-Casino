d7552ee2c73bd835a49333d9cf5846893b1a4545
moffd234 <159962112+moffd234@users.noreply.github.com>
2025-04-08 21:09:55 -0400

    wrote get_turn

--- Application/Casino/Games/RockPaperScissors/RPS.py
+++ Application/Casino/Games/RockPaperScissors/RPS.py
@@ -10,3 +10,12 @@ class RPS(Game):
 
     def __init__(self, player, manager):
         super().__init__(player, manager)
+
+
+    def get_turn(self) -> str:
+        turn: str = self.console.get_string_input("Enter your turn: (Rock, Paper, Scissors)")
+
+        while turn != "Rock" and turn != "Paper" and turn != "Scissors":
+            turn: str = self.console.get_string_input("Enter your turn: (Rock, Paper, Scissors)")
+
+        return turn
