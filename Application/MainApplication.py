e8554d04c3b9dc23d479c0d04a206e3a04948ae4
Daniel Moffett <moffd234@gmail.com>
2025-04-10 17:36:49 -0400

    Created db initializer

--- Application/MainApplication.py
+++ Application/MainApplication.py
@@ -1,5 +1,7 @@
 from Application.Casino.Casino import Casino
+from Application.db import init_db
 
 if __name__ == '__main__':
+    init_db()
     casino = Casino()
     casino.run()
---
+++ Application/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from Casino.UserAccount import Base
+
+engine = create_engine('sqlite:///casino.db', echo=False)
+
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
