9c49c594923cdccf74ae48e2e2ae4279192a11af
Daniel Moffett <moffd234@gmail.com>
2025-04-10 18:11:13 -0400

    Fixed init for UserAccount.py

--- Application/Casino/Accounts/UserAccount.py
+++ Application/Casino/Accounts/UserAccount.py
@@ -1,9 +1,17 @@
+from sqlalchemy.dialects.postgresql import Any
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, String, Float
 
 Base = declarative_base()
 
 class UserAccount(Base):
+
+    def __init__(self, username, password, balance, **kw: Any):
+        super().__init__(**kw)
+        self.username = username
+        self.password = password
+        self.balance = balance
+
     __tablename__ = 'user_account'
     username = Column(String, primary_key=True, nullable=False)
     password = Column(String, nullable=False)
