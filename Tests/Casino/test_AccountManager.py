77f34bf9557610287bba7615c6c54b71d958be0a
Daniel Moffett <moffd234@gmail.com>
2025-04-12 17:27:47 -0400

    Wrote UnitTest for add_and_save and subtract_and_save

--- Tests/Casino/test_AccountManager.py
+++ Tests/Casino/test_AccountManager.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 from Tests.BaseTest import BaseTest
 from Application.Casino.Accounts.UserAccount import UserAccount
 
@@ -42,3 +44,25 @@ class AccountManagerTest(BaseTest):
         actual: UserAccount = self.manager.get_account("this_name_won't_be_used", "secure123")
 
         self.assertIsNone(actual)
+
+    def test_add_winnings_and_save(self):
+        account: UserAccount = self.manager.create_account("test_username", "test_password")
+        self.manager.session.commit = MagicMock()
+        self.manager.add_and_save_account(account, 50.0)
+
+        expected: float = 100
+        actual: float = account.balance
+
+        self.assertEqual(expected, actual)
+        self.manager.session.commit.assert_called_once()
+
+    def test_subtract_and_save(self):
+        account: UserAccount = self.manager.create_account("test_username", "test_password")
+        self.manager.session.commit = MagicMock()
+        self.manager.subtract_and_save_account(account, 50.0)
+
+        expected: float = 0
+        actual: float = account.balance
+
+        self.assertEqual(expected, actual)
+        self.manager.session.commit.assert_called_once()
