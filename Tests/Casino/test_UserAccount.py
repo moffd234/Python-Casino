9968cb3a66eaa5879c6b72cfb3b5c6bcccae39dc
Daniel Moffett <moffd234@gmail.com>
2025-04-12 17:56:57 -0400

    Wrote tests for subtract_losses

--- Tests/Casino/test_UserAccount.py
+++ Tests/Casino/test_UserAccount.py
@@ -12,3 +12,26 @@ class TestUserAccount(BaseTest):
         self.assertEqual(self.account.username, "test_username")
         self.assertEqual(self.account.password, "test_password")
         self.assertEqual(self.account.balance, 50)
+
+    def test_subtract_losses(self):
+        expected: float = self.account.balance - 10
+
+        self.account.subtract_losses(10)
+
+        self.assertEqual(self.account.balance, expected)
+
+    def test_subtract_losses_negative(self):
+        expected: str = "Wager must be positive"
+        with self.assertRaises(ValueError) as ve:
+            self.account.subtract_losses(-10)
+
+        actual: str = ve.exception.args[0]
+        self.assertEqual(expected, actual)
+
+    def test_subtract_losses_negative_balance(self):
+        expected: str = f"Insufficient funds! Available: {self.account.balance}, Tried to subtract: {100}"
+        with self.assertRaises(ValueError) as ve:
+            self.account.subtract_losses(100)
+
+        actual: str = ve.exception.args[0]
+        self.assertEqual(expected, actual)
