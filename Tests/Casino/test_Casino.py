a0e96463e3b953d9b9f70b3b244c678e6222aab2
moffd234 <159962112+moffd234@users.noreply.github.com>
2025-04-14 19:39:11 -0400

    Wrote helper functions to reduce duplicate code

--- Tests/Casino/test_Casino.py
+++ Tests/Casino/test_Casino.py
@@ -25,23 +25,25 @@ class test_Casino(BaseTest):
         actual: str = self.casino.print_welcome()
         self.assertEqual(expected.strip(), actual.strip())
 
+    def assert_account_info(self, account):
+        expected_username = "test_username"
+        expected_password = "test_password"
+        expected_balance = 50.0
+        actual_username = account.username
+        actual_password = account.password
+        actual_balance = account.balance
+        self.assertEqual(expected_username, actual_username)
+        self.assertEqual(expected_password, actual_password)
+        self.assertEqual(expected_balance, actual_balance)
+
+
     @patch("Application.Casino.Accounts.AccountManager.AccountManager.get_account",
            return_value=UserAccount("test_username", "test_password", 50.0))
     @patch("Application.Utils.IOConsole.IOConsole.get_string_input", side_effect=["test_username", "test_password"])
     def test_handle_login(self, mock_inputs, mock_get_account):
         account: UserAccount = self.casino.handle_login()
 
-        expected_username = "test_username"
-        expected_password = "test_password"
-        expected_balance = 50.0
-
-        actual_username = account.username
-        actual_password = account.password
-        actual_balance = account.balance
-
-        self.assertEqual(expected_username, actual_username)
-        self.assertEqual(expected_password, actual_password)
-        self.assertEqual(expected_balance, actual_balance)
+        self.assert_account_info(account)
 
     @patch("Application.Casino.Accounts.AccountManager.AccountManager.get_account", return_value=None)
     @patch("Application.Utils.IOConsole.IOConsole.get_string_input", side_effect=["wrong_user", "wrong_pass"] * 5)
@@ -55,17 +57,7 @@ class test_Casino(BaseTest):
     def test_handle_signup(self, mock_inputs, mock_get_account):
         account: UserAccount = self.casino.handle_signup()
 
-        expected_username = "test_username"
-        expected_password = "test_password"
-        expected_balance = 50.0
-
-        actual_username = account.username
-        actual_password = account.password
-        actual_balance = account.balance
-
-        self.assertEqual(expected_username, actual_username)
-        self.assertEqual(expected_password, actual_password)
-        self.assertEqual(expected_balance, actual_balance)
+        self.assert_account_info(account)
 
     @patch("builtins.print")
     @patch("builtins.input", return_value="50")
@@ -82,47 +74,25 @@ class test_Casino(BaseTest):
     @patch("builtins.print")
     @patch("builtins.input", side_effect=["-1", "50"])
     def test_handle_add_funds_negative(self, mock_input, mock_print):
-        expected_balance: float = self.casino.account.balance + 50
-        self.casino.add_funds()
-
-        actual_balance: float = self.casino.account.balance
-
-        mock_print.assert_has_calls([
-            call(self.casino.console.print_colored("Invalid input. Please try again", ANSI_COLORS.RED)),
-            call(
-                f"{ANSI_COLORS.GREEN.value}You have added $50.0 to your funds! New Balance is {self.casino.account.balance}")
-        ])
-
-        self.assertEqual(expected_balance, actual_balance)
+        self.add_funds_and_assert(mock_print)
 
     @patch("builtins.print")
     @patch("builtins.input", side_effect=[".001", "50"])
     def test_handle_add_funds_low_decimal(self, mock_input, mock_print):
+        self.add_funds_and_assert(mock_print)
+
+    @patch("builtins.print")
+    @patch("builtins.input", side_effect=["1000", "50"])
+    def test_handle_add_funds_1000(self, mock_input, mock_print):
+        self.add_funds_and_assert(mock_print)
+
+    def add_funds_and_assert(self, mock_print):
         expected_balance: float = self.casino.account.balance + 50
         self.casino.add_funds()
-
         actual_balance: float = self.casino.account.balance
-
         mock_print.assert_has_calls([
             call(self.casino.console.print_colored("Invalid input. Please try again", ANSI_COLORS.RED)),
             call(
                 f"{ANSI_COLORS.GREEN.value}You have added $50.0 to your funds! New Balance is {self.casino.account.balance}")
         ])
-
-        self.assertEqual(expected_balance, actual_balance)
-
-    @patch("builtins.print")
-    @patch("builtins.input", side_effect=["1000", "75"])
-    def test_handle_add_funds_1000(self, mock_input, mock_print):
-        expected_balance: float = self.casino.account.balance + 75
-        self.casino.add_funds()
-
-        actual_balance: float = self.casino.account.balance
-
-        mock_print.assert_has_calls([
-            call(self.casino.console.print_colored("Invalid input. Please try again", ANSI_COLORS.RED)),
-            call(
-                f"{ANSI_COLORS.GREEN.value}You have added $75.0 to your funds! New Balance is {self.casino.account.balance}")
-        ])
-
         self.assertEqual(expected_balance, actual_balance)
