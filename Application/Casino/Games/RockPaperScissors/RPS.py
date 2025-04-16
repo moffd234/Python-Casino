1a383525e6167c0bb1608b9fa00fc7a7117aa89f
Daniel Moffett <moffd234@gmail.com>
2025-04-16 17:19:55 -0400

    Wrote handle_winner

--- Application/Casino/Games/RockPaperScissors/RPS.py
+++ Application/Casino/Games/RockPaperScissors/RPS.py
@@ -1,5 +1,4 @@
 from random import randint
-
 from Application.Casino.Accounts.AccountManager import AccountManager
 from Application.Casino.Accounts.UserAccount import UserAccount
 from Application.Casino.Games.Game import Game
@@ -14,6 +13,24 @@ def get_comp_turn() -> str:
     else:
         return "Rock"
 
+
+def handle_winner(comp_turn: str, user_turn: str) -> str:
+    if comp_turn == user_turn:
+        return f"Draw! {user_turn} ties {comp_turn}!"
+
+    winners: dict = {
+        "Rock": "Scissors",
+        "Paper": "Rock",
+        "Scissors": "Paper"
+    }
+
+    if winners[user_turn] == winners[comp_turn]:
+        return f"You won! {user_turn} beats {comp_turn}!"
+
+    # ASSERT: CPU has winning move
+    return f"You lost! {user_turn} loses to {comp_turn}!"
+
+
 class RPS(Game):
     def print_welcome_message(self) -> str:
         pass
@@ -25,7 +42,7 @@ class RPS(Game):
         super().__init__(player, manager)
 
 
-    def get_turn(self) -> str:
+    def get_user_turn(self) -> str:
         turn: str = self.console.get_string_input("Enter your turn: (Rock, Paper, Scissors)")
 
         while turn != "Rock" and turn != "Paper" and turn != "Scissors":
