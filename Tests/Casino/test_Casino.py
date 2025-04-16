b6975f074782524278ee570b23d0d6ac717ab59c
Daniel Moffett <moffd234@gmail.com>
2025-04-16 16:58:03 -0400

    Fixed casino unit tests

--- Tests/Casino/test_Casino.py
+++ Tests/Casino/test_Casino.py
@@ -122,38 +122,39 @@ class test_Casino(BaseTest):
             self.casino.console.print_colored(f"Your password has been updated!", ANSI_COLORS.GREEN))
         self.assertEqual(expected_password, actual_password)
 
-    @patch("builtins.print")
+    @patch("Application.Utils.IOConsole.IOConsole.print_error")
     @patch("builtins.input", side_effect=["wrong_password"] * 5)
-    def test_reset_password_failed_times(self, mock_input, mock_print):
+    def test_reset_password_failed_times(self, mock_input, mock_print_error):
         expected_password: str = self.casino.account.password
         self.casino.reset_password()
 
         actual_password = self.casino.account.password
 
-        mock_print.assert_has_calls([
-            # 5 incorrect password messages and 1 lockout message
-            call(self.casino.console.print_colored("Passwords do not match", ANSI_COLORS.RED)),
-            call(self.casino.console.print_colored("Passwords do not match", ANSI_COLORS.RED)),
-            call(self.casino.console.print_colored("Passwords do not match", ANSI_COLORS.RED)),
-            call(self.casino.console.print_colored("Passwords do not match", ANSI_COLORS.RED)),
-            call(self.casino.console.print_colored("Passwords do not match", ANSI_COLORS.RED)),
-            call(self.casino.console.print_colored("Too many attempts try again later", ANSI_COLORS.RED))
+        # 5 incorrect password messages and 1 lockout message
+        mock_print_error.assert_has_calls([
+            call("Passwords do not match"),
+            call("Passwords do not match"),
+            call("Passwords do not match"),
+            call("Passwords do not match"),
+            call("Passwords do not match"),
+            call("Too many invalid attempts. Please try again")
         ])
         self.assertEqual(expected_password, actual_password)
 
     @patch("builtins.print")
+    @patch("Application.Utils.IOConsole.IOConsole.print_error")
     @patch("builtins.input", side_effect=["wrong_password", "wrong_password", "password", "new_password"])
-    def test_reset_password_failed_then_works(self, mock_input, mock_print):
+    def test_reset_password_failed_then_works(self, mock_input,mock_print_error, mock_print):
         self.casino.reset_password()
 
         expected_password: str = "new_password"
         actual_password = self.casino.account.password
 
-        mock_print.assert_has_calls([
-            # 5 incorrect password messages and 1 lockout message
-            call(self.casino.console.print_colored("Passwords do not match", ANSI_COLORS.RED)),
-            call(self.casino.console.print_colored("Passwords do not match", ANSI_COLORS.RED)),
-            call(self.casino.console.print_colored(f"Your password has been updated!", ANSI_COLORS.GREEN))
+        mock_print.assert_called_once_with((self.casino.console.print_colored("Your password has been updated!",
+                                                                              ANSI_COLORS.GREEN)))
+        mock_print_error.assert_has_calls([
+            call("Passwords do not match"),
+            call("Passwords do not match")
         ])
         self.assertEqual(expected_password, actual_password)
 
