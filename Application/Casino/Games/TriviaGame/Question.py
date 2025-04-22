84fd2144ec7555d166145996f5a848205de01ef3
Daniel Moffett <moffd234@gmail.com>
2025-04-22 17:24:15 -0400

    Fixed type hints from [str] to list[str]

--- Application/Casino/Games/TriviaGame/Question.py
+++ Application/Casino/Games/TriviaGame/Question.py
@@ -2,9 +2,9 @@ import random
 
 
 class Question:
-    def __init__(self, question: str, answer: str, wrong_answers: [str]):
+    def __init__(self, question: str, answer: str, wrong_answers: list[str]):
         self.question: str = question
         self.answer: str = answer
-        self.wrong_answers: [str] = wrong_answers
-        self.all_options: [str] = self.wrong_answers + [self.answer]
+        self.wrong_answers: list[str] = wrong_answers
+        self.all_options: list[str] = self.wrong_answers + [self.answer]
         random.shuffle(self.all_options)
