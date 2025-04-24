06a0dc9bc73c123f9ab31efb5947211aac0bfc35
Daniel Moffett <moffd234@gmail.com>
2025-04-24 16:14:26 -0400

    Finished tests for get_possible_categories

--- Application/Casino/Games/TriviaGame/TriviaGame.py
+++ Application/Casino/Games/TriviaGame/TriviaGame.py
@@ -151,7 +151,7 @@ class TriviaGame(Game):
         valid_cats = self.get_valid_categories(self.difficulty)
         self.cat: Category = self.get_category(valid_cats)
 
-    def get_possible_categories(self) -> list | None:
+    def get_possible_categories(self) -> list[Category] | None:
         cached_categories: dict | None = cache_loader()
 
         if cached_categories:
