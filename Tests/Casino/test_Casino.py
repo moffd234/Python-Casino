4b4f06ed550f1110f69a07dc2868bacbcedf244d
Daniel Moffett <moffd234@gmail.com>
2025-04-26 23:28:09 -0400

    Added alternative input options for numberguess and rps

--- Tests/Casino/test_Casino.py
+++ Tests/Casino/test_Casino.py
@@ -307,6 +307,11 @@ class TestCasino(BaseTest):
 
         mock_print.assert_called_once_with("Invalid input. Please try again\n\n")
 
+    @patch("Application.Utils.IOConsole.IOConsole.get_string_input", side_effect=["rps", "back"])
+    @patch("Application.Casino.Games.RockPaperScissors.RPS.RPS.run", return_value=None)
+    def test_prompt_game_rps(self, mock_run, mock_input):
+        self.assert_prompt_game(mock_input, mock_run)
+
     def assert_prompt_game(self, mock_input, mock_run):
         self.casino.prompt_game()
         mock_input.assert_has_calls([call("Welcome to the Game Selection Dashboard!" +
