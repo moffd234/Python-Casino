e3f2e9c56b19d1a158cf0643f510c74ed46c848c
Daniel Moffett <moffd234@gmail.com>
2025-04-28 17:49:33 -0400

    Wrote print_welcome_message

--- Application/Casino/Games/Slots/Slots.py
+++ Application/Casino/Games/Slots/Slots.py
@@ -9,7 +9,22 @@ class Slots(Game):
         super().__init__(player, manager)
 
     def print_welcome_message(self) -> str:
-        pass
+        return r"""        
+        Yb        dP 888888 88      dP""b8  dP"Yb  8b    d8 888888     888888  dP"Yb      .dP"Y8 88      dP"Yb  888888 .dP"Y8 
+         Yb  db  dP  88__   88     dP   `" dP   Yb 88b  d88 88__         88   dP   Yb     `Ybo." 88     dP   Yb   88   `Ybo." 
+          YbdPYbdP   88""   88  .o Yb      Yb   dP 88YbdP88 88""         88   Yb   dP     o.`Y8b 88  .o Yb   dP   88   o.`Y8b 
+           YP  YP    888888 88ood8  YboodP  YbodP  88 YY 88 888888       88    YbodP      8bodP' 88ood8  YbodP    88   8bodP' 
+           
+           Rules:
+                - 1. Enter a wager amount.
+                - 2. Match three symbols on the pay line to win
+                - 3. Payouts vary based on the symbols matched:
+                     - Three 7s: Jackpot(10x)
+                     - Three Bells: Big Win(5x)
+                     - Three Bars: Medium Win (2x)
+                     - Three Cherries: Small Win (1.5x)
+                     - Any other combination: No Win (You lose your wager)
+        """
 
     def run(self):
         pass
