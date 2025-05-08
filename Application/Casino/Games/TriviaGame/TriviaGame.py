d355041b38ffea75287f6095d7299595ec583e5f
Daniel Moffett <moffd234@gmail.com>
2025-05-08 15:45:51 -0400

    Removed print_colored when printing welcome in TriviaGame.py

--- Application/Casino/Games/TriviaGame/TriviaGame.py
+++ Application/Casino/Games/TriviaGame/TriviaGame.py
@@ -89,7 +89,7 @@ class TriviaGame(Game):
 
     def run(self):
 
-        self.console.print_colored(self.print_welcome_message())
+        self.print_welcome_message()
 
         while self.get_continue_input():
             wager: float = self.get_wager_amount()
