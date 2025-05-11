95a83ed64d8b75b5460d3cd149d7c426e4af66b1
Daniel Moffett <moffd234@gmail.com>
2025-05-10 20:53:56 -0400

    Updated model to include security questions, email, and UUID details

--- Application/Casino/Accounts/UserAccount.py
+++ Application/Casino/Accounts/UserAccount.py
@@ -1,20 +1,34 @@
 from sqlalchemy.dialects.postgresql import Any
 from Application.Casino.Accounts.db import Base
-from sqlalchemy import Column, String, Float
+from sqlalchemy import Column, String, Float, DateTime
 
 
 class UserAccount(Base):
 
-    def __init__(self, username: str, password: str, balance: float, **kw: Any):
+    def __init__(self, username: str, password: str, balance: float, email: str, questions: dict[str, str], **kw: Any):
         super().__init__(**kw)
         self.username: str = username
         self.password: str = password
         self.balance: float = balance
+        self.email: str = email
+        question_keys = list(questions.keys())
+        self.security_question_one = question_keys[0]
+        self.security_question_two = question_keys[1]
+        self.security_answer_one = questions[self.security_question_one]
+        self.security_answer_two = questions[self.security_question_two]
 
     __tablename__ = 'user_account'
     username = Column(String, primary_key=True, nullable=False)
     password = Column(String, nullable=False)
     balance = Column(Float, default=0.0, nullable=False)
+    email = Column(String, nullable=False)
+    security_question_one = Column(String, nullable=False)
+    security_question_two = Column(String, nullable=False)
+    security_answer_one = Column(String, nullable=False)
+    security_answer_two = Column(String, nullable=False)
+    reset_token = Column(String, nullable=True)
+    reset_token_expiration = Column(DateTime, nullable=True)
+
 
     def subtract_losses(self, wager: float) -> None:
         if wager <= 0:
