afa87051ef82b05ce0084c06e01bfa699154d4c3
Daniel Moffett <moffd234@gmail.com>
2025-05-10 20:57:56 -0400

    Refactored questions to use a list instead of dict

--- Application/Casino/Accounts/UserAccount.py
+++ Application/Casino/Accounts/UserAccount.py
@@ -5,17 +5,16 @@ from sqlalchemy import Column, String, Float, DateTime
 
 class UserAccount(Base):
 
-    def __init__(self, username: str, password: str, balance: float, email: str, questions: dict[str, str], **kw: Any):
+    def __init__(self, username: str, password: str, balance: float, email: str, questions: list[str], **kw: Any):
         super().__init__(**kw)
         self.username: str = username
         self.password: str = password
         self.balance: float = balance
         self.email: str = email
-        question_keys = list(questions.keys())
-        self.security_question_one = question_keys[0]
-        self.security_question_two = question_keys[1]
-        self.security_answer_one = questions[self.security_question_one]
-        self.security_answer_two = questions[self.security_question_two]
+        self.security_question_one = questions[0]
+        self.security_answer_one = questions[1]
+        self.security_question_two = questions[2]
+        self.security_answer_two = questions[3]
 
     __tablename__ = 'user_account'
     username = Column(String, primary_key=True, nullable=False)
