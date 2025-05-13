fd8d500c7fb0f0501564ffb01458d63951759f82
Daniel Moffett <moffd234@gmail.com>
2025-05-13 17:24:53 -0400

    Add reset_token and reset_token_expiration to db

--- Application/Casino/Accounts/UserAccount.py
+++ Application/Casino/Accounts/UserAccount.py
@@ -1,11 +1,10 @@
-from sqlalchemy.dialects.postgresql import Any
 from Application.Casino.Accounts.db import Base
-from sqlalchemy import Column, String, Float, DateTime
+from sqlalchemy import Column, String, Float, DateTime, UUID
 
 
 class UserAccount(Base):
 
-    def __init__(self, username: str, password: str, balance: float, email: str, questions: list[str], **kw: Any):
+    def __init__(self, username: str, password: str, balance: float, email: str, questions: list[str], **kw):
         super().__init__(**kw)
         self.username: str = username
         self.password: str = password
@@ -15,8 +14,11 @@ class UserAccount(Base):
         self.security_answer_one = questions[1]
         self.security_question_two = questions[2]
         self.security_answer_two = questions[3]
+        self.reset_token = None
+        self.reset_token_expiration = None
 
     __tablename__ = 'user_account'
+
     username = Column(String, primary_key=True, nullable=False)
     password = Column(String, nullable=False)
     balance = Column(Float, default=0.0, nullable=False)
@@ -25,7 +27,7 @@ class UserAccount(Base):
     security_question_two = Column(String, nullable=False)
     security_answer_one = Column(String, nullable=False)
     security_answer_two = Column(String, nullable=False)
-    reset_token = Column(String, nullable=True)
+    reset_token = Column(UUID(as_uuid=True), nullable=True)
     reset_token_expiration = Column(DateTime, nullable=True)
 
 
