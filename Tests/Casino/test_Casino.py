9ceff64067ca14df1c9e4e751f590bbc7bec6a01
Daniel Moffett <moffd234@gmail.com>
2025-05-18 16:14:31 -0400

    Wrote tests for reset_from_login

--- Tests/Casino/test_Casino.py
+++ Tests/Casino/test_Casino.py
@@ -978,3 +978,32 @@ class TestCasino(BaseTest):
                                       call(self.casino.account.security_question_two,
                                            self.casino.account.security_answer_two)])
         self.assertFalse(actual)
+
+    @patch(f"{CASINO_CLASS_PATH}.prompt_and_check_email", return_value=True)
+    @patch(f"{CASINO_CLASS_PATH}.get_security_answers", return_value=True)
+    @patch(f"{CASINO_CLASS_PATH}.validate_and_reset")
+    @patch(f"{ACCOUNT_MANAGER_CLASS_PATH}.email_recovery_token")
+    def test_reset_from_login_true(self, mock_email, mock_validate, mock_get_answers, mock_prompt_email):
+        actual: bool = self.casino.reset_from_login()
+
+        mock_prompt_email.assert_called_once()
+        mock_get_answers.assert_called_once()
+        mock_validate.assert_called_once()
+        mock_email.assert_called_once()
+        self.assertTrue(actual)
+
+    @patch(f"{CASINO_CLASS_PATH}.prompt_and_check_email", return_value=False)
+    def test_reset_from_login_fail_prompt_email(self, mock_prompt_email):
+        actual: bool = self.casino.reset_from_login()
+
+        mock_prompt_email.assert_called_once()
+        self.assertFalse(actual)
+
+    @patch(f"{CASINO_CLASS_PATH}.prompt_and_check_email", return_value=True)
+    @patch(f"{CASINO_CLASS_PATH}.get_security_answers", return_value=False)
+    def test_reset_from_login_fail_security_questions(self, mock_get_answers, mock_prompt_email):
+        actual: bool = self.casino.reset_from_login()
+
+        mock_prompt_email.assert_called_once()
+        mock_get_answers.assert_called_once()
+        self.assertFalse(actual)
